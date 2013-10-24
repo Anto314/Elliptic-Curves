aa6731ce093328d8c627b8c38feb99727f22fa8c
ar <ar@atom.home>
2013-10-24 16:37:52 +0200

    Working DSA.

--- Sources/DSA.c
+++ Sources/DSA.c
@@ -9,79 +9,184 @@
 #include "Network.h"
 #include "Utils.h"
 
-static void DSAAlice(TEllipticCurve *Pointer_Curve, unsigned char *Pointer_Message, size_t Message_Length, int Socket_Bob, mpz_t Private_Key_Alice)
+/** Maximum number of bytes (including the trailing zero) of the message. */
+#define MAXIMUM_MESSAGE_SIZE 2048
+
+/** Check if the number is comprised between 1 and Number_Order - 1.
+ * @param Number The number to check.
+ * @param Number_Order The order of the group.
+ * @return 1 if the number is in bounds or 0 if not.
+ */
+static int IsNumberInBounds(mpz_t Number, mpz_t Number_Order)
+{
+	if (mpz_cmp_ui(Number, 1) < 0) return 0;
+	if (mpz_cmp(Number, Number_Order) >= 0) return 0;
+	return 1;
+}
+
+static void DSAAlice(TEllipticCurve *Pointer_Curve, unsigned char *Pointer_Message, size_t Message_Length, mpz_t Private_Key_Alice, mpz_t Output_Number_U, mpz_t Output_Number_V)
 {
 	unsigned char Buffer_Hash[UTILS_HASH_LENGTH];
-	mpz_t Number_Temp, Number_R, Number_S, Number_Hash;
+	mpz_t Number_Temp, Number_Hash, Number_Random;
 	TPoint Point;
 	
 	// Initialize variables
 	mpz_init(Number_Temp);
-	mpz_init(Number_R);
-	mpz_init(Number_S);
 	mpz_init(Number_Hash);
+	mpz_init(Number_Random);
 	PointCreate(0, 0, &Point);
 	
 	// Compute message hash
 	printf("Alice is computing message hash...\n");
 	UtilsComputeHash(Pointer_Message, Message_Length, Buffer_Hash);
-	mpz_set_str(Number_Hash, Buffer_Hash, 10);
+	mpz_set_str(Number_Hash, (char *) Buffer_Hash, 10);
 	UtilsShowHash(Buffer_Hash);
 	putchar('\n');
 	
+	// Generate signature pair (u, v)
 	while (1)
 	{
-		// Get a random K
-		UtilsGenerateRandomNumber(Pointer_Curve->p, Number_R);
+		// Get a random K between 1 et n - 1
+		do
+		{
+			UtilsGenerateRandomNumber(Pointer_Curve->n, Number_Random);
+		} while (!IsNumberInBounds(Number_Random, Pointer_Curve->n));
 		
 		// Compute a curve point
-		ECMultiplication(Pointer_Curve, &Pointer_Curve->Point_Generator, Number_R, &Point);
+		ECMultiplication(Pointer_Curve, &Pointer_Curve->Point_Generator, Number_Random, &Point);
 	
-		// Calculate R
-		mpz_mod(Number_R, Point.X, &Pointer_Curve->p);
-		// Retry if the computed R is 0
-		if (mpz_cmp_ui(Number_R, 0) == 0) continue;
+		// Calculate 'u'
+		mpz_mod(Output_Number_U, Point.X, Pointer_Curve->n);
+		// Retry if the computed 'u' is 0
+		if (mpz_cmp_ui(Output_Number_U, 0) == 0) continue;
 		
-		// Calculate S
-		mpz_mul(Number_Temp, Number_R, Private_Key_Alice); // r * dA
-		mpz_add(Number_Hash, Number_Temp, Number_Hash); // z + (r * dA), recycle Hash variable
-		mpz_invert(
+		// Calculate 'v'
+		mpz_mul(Number_Temp, Output_Number_U, Private_Key_Alice); // u * s
+		mpz_add(Number_Temp, Number_Temp, Number_Hash); // H(m) + (u * s)
+		mpz_invert(Output_Number_V, Number_Random, Pointer_Curve->n); // Compute k^-1 mod n
+		mpz_mul(Number_Temp, Output_Number_V, Number_Temp); // (k^-1) * (H(m) + (u * s))
+		mpz_mod(Output_Number_V, Number_Temp, Pointer_Curve->n); // (k^-1) * (H(m) + (u * s)) mod n
 		
-		// Retry if the computed S is 0
-		if (mpz_cmp_ui(Number_S, 0) != 0) break;
+		// Retry if the computed 'v' is 0
+		if (mpz_cmp_ui(Output_Number_V, 0) != 0) break;
 	}
 	
-	
+	// Display signature pair
+	gmp_printf("Signature :\nu = %Zd\nv = %Zd\n\n", Output_Number_U, Output_Number_V);
 	
 	// Free resources
 	mpz_clear(Number_Temp);
-	mpz_clear(Number_R);
-	mpz_clear(Number_S);
 	mpz_clear(Number_Hash);
+	mpz_clear(Number_Random);
+	PointFree(&Point);
 }
 
-static void DSABob(TEllipticCurve *Pointer_Curve, int Socket_Alice)
+static int DSABob(TEllipticCurve *Pointer_Curve, unsigned char *Pointer_Message, size_t Message_Length, TPoint *Pointer_Public_Key_Alice, mpz_t Number_U, mpz_t Number_V)
 {
-	TPoint Point_Public_Key_Alice;
+	unsigned char Buffer_Hash[UTILS_HASH_LENGTH];
+	mpz_t Number_Hash, Number_Temp;
+	TPoint Point_Temp, Point_Temp_2;
+	int Return_Value;
 	
 	// Initialize variables
-	PointCreate(0, 0, &Point_Public_Key_Alice);
+	mpz_init(Number_Hash);
+	mpz_init(Number_Temp);
+	PointCreate(0, 0, &Point_Temp);
+	PointCreate(0, 0, &Point_Temp_2);
+		
+	// Check parameters correctness
+	printf("Bob is checking parameters correctness... ");
+	fflush(stdout);
 	
-	// Receiving Alice's public key
-	printf("Waiting for Alice's public key...\n");
-	NetworkReceivePoint(Socket_Alice, &Point_Public_Key_Alice);
-	PointShow(&Point_Public_Key_Alice);
+	// 'u' must be between 1 and n - 1)
+	if (!IsNumberInBounds(Number_U, Pointer_Curve->n))
+	{
+		printf("\nError : 'u' is not in [1; n - 1].\n");
+		return 0;
+	}
+
+	// 'v' must be between 1 and n - 1)
+	if (!IsNumberInBounds(Number_V, Pointer_Curve->n))
+	{
+		printf("\nError : 'v' is not in [1; n - 1].\n");
+		return 0;
+	}
+	
+	// Alice's public key (Q) must not be equal to (0, 0)
+	if (Pointer_Public_Key_Alice->Is_Infinite)
+	{
+		printf("\nError : Alice's public key is equal to (0, 0).\n");
+		return 0;
+	}
+	
+	// n * Q must be equal to (0, 0)
+	ECMultiplication(Pointer_Curve, Pointer_Public_Key_Alice, Pointer_Curve->n, &Point_Temp);
+	if (!Point_Temp.Is_Infinite)
+	{
+		printf("\nError : n.Q != (0, 0).\n");
+		PointShow(&Point_Temp);
+		return 0;
+	}
+	printf("done.\n\n");
+	
+	// Compute message hash
+	printf("Bob is computing message hash...\n");
+	UtilsComputeHash(Pointer_Message, Message_Length, Buffer_Hash);
+	mpz_set_str(Number_Hash, (char *) Buffer_Hash, 10);
+	UtilsShowHash(Buffer_Hash);
 	putchar('\n');
+	
+	printf("Bob is checking signature...\n");
+	// Compute (H(m) / v) mod n using v^-1
+	mpz_invert(Number_V, Number_V, Pointer_Curve->n); // Only v^-1 is used by other calculations
+	mpz_mul(Number_Temp, Number_Hash, Number_V);
+	mpz_mod(Number_Temp, Number_Temp, Pointer_Curve->n);
+
+	// Compute (H(m) / v mod n) * P
+	ECMultiplication(Pointer_Curve, &Pointer_Curve->Point_Generator, Number_Temp, &Point_Temp);
+	
+	// Compute u / v mod n using v^-1
+	mpz_mul(Number_Temp, Number_U, Number_V);
+	mpz_mod(Number_Temp, Number_Temp, Pointer_Curve->n);
+	
+	// Multiply to public key Q
+	ECMultiplication(Pointer_Curve, Pointer_Public_Key_Alice, Number_Temp, &Point_Temp_2);
+	
+	// Add the two points
+	ECAddition(Pointer_Curve, &Point_Temp, &Point_Temp_2, &Point_Temp);
+	
+	// Is resulting point X coordinate equal to u ?
+	mpz_mod(Number_Temp, Point_Temp.X, Pointer_Curve->n);
+	gmp_printf("u = %Zd\nResult = %Zd\n", Number_U, Number_Temp);
+	if (mpz_cmp(Number_U, Number_Temp) == 0)
+	{
+		printf("\033[32mSUCCESS : signature matched.\n");
+		Return_Value = 1;
+	}
+	else
+	{
+		printf("\033[31mFAILURE : bad signature.\n");
+		Return_Value = 0;
+	}
+	// Reset color attributes
+	printf("\033[0m");
+	
+	// Free resources
+	mpz_clear(Number_Hash);
+	mpz_clear(Number_Temp);
+	PointFree(&Point_Temp);
+	PointFree(&Point_Temp_2);
+	
+	return Return_Value;
 }
 
 int main(int argc, char *argv[])
 {
-	char Is_Alice, *String_Parameter_Character, *String_Parameter_File_Name, *String_Parameter_IP_Address;
+	char Is_Alice, *String_Parameter_Character, *String_Parameter_File_Name, *String_Parameter_IP_Address, Message[MAXIMUM_MESSAGE_SIZE];
 	unsigned short Port;
 	TEllipticCurve Curve;
-	int Socket_Alice, Socket_Bob;
-	unsigned char Message[] = "Ciao, codesto messaggio sa di merda";
-	mpz_t Private_Key_Alice;
+	int Socket_Alice, Socket_Bob, Message_Length;
+	mpz_t Private_Key_Alice, Signature_Number_U, Signature_Number_V;
 	TPoint Point_Public_Key_Alice;
 	
 	// Check parameters
@@ -117,6 +222,11 @@ int main(int argc, char *argv[])
 	
 	UtilsInitializeRandomGenerator();
 	
+	// Initialize variables
+	mpz_init(Signature_Number_U);
+	mpz_init(Signature_Number_V);
+	PointCreate(0, 0, &Point_Public_Key_Alice);
+	
 	// Alice
 	if (Is_Alice)
 	{
@@ -142,15 +252,19 @@ int main(int argc, char *argv[])
 		}
 		printf("Bob is connected.\n\n");
 		
-		// Generate Alice's private key once
-		printf("Computing Alice's private key...\n");
+		// Initialize variables
 		mpz_init(Private_Key_Alice);
-		UtilsGenerateRandomNumber(Curve.p, Private_Key_Alice);
+		
+		// Generate Alice's private key 's' once
+		printf("Computing Alice's private key...\n");
+		do
+		{
+			UtilsGenerateRandomNumber(Curve.n, Private_Key_Alice);
+		} while (!IsNumberInBounds(Private_Key_Alice, Curve.n));
 		gmp_printf("%Zd\n\n", Private_Key_Alice);
 		
-		// Generate public key = Private_Key * Curve_Generator
+		// Generate public key 'Q' = private key * curve point
 		printf("Computing Alice's public key...\n");
-		PointCreate(0, 0, &Point_Public_Key_Alice);
 		ECMultiplication(&Curve, &Curve.Point_Generator, Private_Key_Alice, &Point_Public_Key_Alice);
 		PointShow(&Point_Public_Key_Alice);
 		putchar('\n');
@@ -159,10 +273,32 @@ int main(int argc, char *argv[])
 		printf("Sending public key to Bob... ");
 		fflush(stdout);
 		NetworkSendPoint(Socket_Bob, &Point_Public_Key_Alice);
-		printf("done.\n");
-		s		
-		DSAAlice(&Curve, Message, sizeof(Message) - 1, Socket_Bob, Private_Key_Alice);
+		printf("done.\n\n");
 		
+		// Create message
+		strcpy(Message, "Ceci est un magnifique message de test.");
+		Message_Length = strlen(Message) + 1; // +1 for terminating zero
+
+		// Send message to Bob
+		printf("Sending message to Bob...\n%s\n\n", Message);
+		fflush(stdout);
+		// Send message size
+		write(Socket_Bob, &Message_Length, sizeof(Message_Length));
+		// Send message content
+		write(Socket_Bob, Message, Message_Length);
+		
+		// Compute 'u' and 'v' numbers used to sign the message
+		DSAAlice(&Curve, (unsigned char *) Message, Message_Length, Private_Key_Alice, Signature_Number_U, Signature_Number_V);
+				
+		// Send 'u' and 'v' to Bob
+		printf("Sending signature numbers to Bob... ");
+		fflush(stdout);
+		NetworkSendMPZ(Socket_Bob, Signature_Number_U);
+		NetworkSendMPZ(Socket_Bob, Signature_Number_V);
+		printf("done.\n");
+		
+		// Free resources
+		mpz_clear(Private_Key_Alice);
 		close(Socket_Bob);
 	}
 	// Bob
@@ -178,10 +314,39 @@ int main(int argc, char *argv[])
 		}
 		printf("Connected to Alice.\n\n");
 		
-		DSABob(&Curve, Socket_Alice);
+		// Receive Alice's public key
+		printf("Waiting for Alice's public key...\n");
+		NetworkReceivePoint(Socket_Alice, &Point_Public_Key_Alice);
+		PointShow(&Point_Public_Key_Alice);
+		putchar('\n');
+		
+		// Receive message length
+		printf("Receiving Alice's message...\n");
+		read(Socket_Alice, &Message_Length, sizeof(Message_Length));
+		if (Message_Length > MAXIMUM_MESSAGE_SIZE)
+		{
+			printf("Error : the message is bigger than the buffer.\n");
+			goto Exit;
+		}
+		// Receive message content
+		read(Socket_Alice, Message, Message_Length);
+		printf("%s\n\n", Message);
+		
+		// Receive signature
+		printf("Receiving signature...\n");
+		NetworkReceiveMPZ(Socket_Alice, Signature_Number_U);
+		NetworkReceiveMPZ(Socket_Alice, Signature_Number_V);
+		gmp_printf("r = %Zd\ns = %Zd\n\n", Signature_Number_U, Signature_Number_V);
+		
+		DSABob(&Curve, (unsigned char *) Message, Message_Length, &Point_Public_Key_Alice, Signature_Number_U, Signature_Number_V);
 	}
 	
+Exit:
+	// Free resources
 	close(Socket_Alice);
 	ECFree(&Curve);
+	mpz_clear(Signature_Number_U);
+	mpz_clear(Signature_Number_V);
+	PointFree(&Point_Public_Key_Alice);
 	return 0;
 }
